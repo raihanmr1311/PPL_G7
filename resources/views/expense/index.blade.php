329499f6ce656369a5a0334c0fe9b74334ffab8b
raihanmr1311 <202410101003@mail.unej.ac.id>
2022-04-26 20:26:18 +0700

    Delete create button in every table list

--- resources/views/expense/index.blade.php
+++ resources/views/expense/index.blade.php
@@ -12,10 +12,6 @@
       <div class="row">
         <div class="col-12">
           <div class="card">
-            <div class="card-header">
-              <a href="{{ route('expenses.create') }}" class="btn btn-primary"> <i class="fa fa-plus"></i> Tambah
-                Pengeluaran</a>
-            </div>
             <div class="card-body">
               <div class="table-responsive">
                 <table class="table table-striped" id="table-1">
--- resources/views/income/index.blade.php
+++ resources/views/income/index.blade.php
@@ -12,9 +12,6 @@
       <div class="row">
         <div class="col-12">
           <div class="card">
-            <div class="card-header">
-              <a href="{{ route('incomes.create') }}" class="btn btn-primary"> <i class="fa fa-plus"></i> Tambah Data Pemasukan</a>
-            </div>
             <div class="card-body">
               <div class="table-responsive">
                 <table class="table table-striped" id="table-1">
