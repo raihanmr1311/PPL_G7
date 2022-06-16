c9a0053e05ca376235a318154d7c5d67760cf58e
raihanmr1311 <202410101003@mail.unej.ac.id>
2022-06-16 16:57:14 +0700

    change title at statistic

--- resources/views/expense/statistic.blade.php
+++ resources/views/expense/statistic.blade.php
@@ -5,7 +5,7 @@
 @section('content')
   <section class="section">
     <div class="section-header">
-      <h1>Statistik</h1>
+      <h1>Statistik Pengeluaran</h1>
     </div>
 
     <div class="section-body">
--- resources/views/income/statistic.blade.php
+++ resources/views/income/statistic.blade.php
@@ -5,7 +5,7 @@
 @section('content')
   <section class="section">
     <div class="section-header">
-      <h1>Statistik</h1>
+      <h1>Statistik Pemasukan</h1>
     </div>
 
     <div class="section-body">
